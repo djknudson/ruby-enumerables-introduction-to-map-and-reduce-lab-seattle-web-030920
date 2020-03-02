d8e55ac833504ed017f41e5d4dfea1abf0d111a4
David Knudson <djknudson@icloud.com>
2020-03-02 10:22:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
 # My Code here....
+def map_to_negativize(source_array)
+  output_array = []
+  source_array.each do |element|
+      output_array << -(element)
+  end
+  output_array
+end
+
+def map_to_no_change(source_array)
+  output_array = Array.new
+  source_array.each do |element|
+    output_array << element
+  end
+  output_array
+end
+
+def map_to_double(source_array)
+  output_array = Array.new
+  source_array.each do |element|
+    output_array << (element * 2)
+  end
+  output_array
+end
+
+def map_to_square(source_array)
+  output_array = Array.new
+  source_array.each do |element|
+    output_array << (element ** 2)
+  end
+  output_array
+end
