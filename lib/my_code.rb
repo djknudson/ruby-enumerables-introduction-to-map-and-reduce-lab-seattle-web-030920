46a554f4a167e63edd7ce3bc7deddc1f492e035b
David Knudson <djknudson@icloud.com>
2020-03-02 10:26:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,7 +31,7 @@ def map_to_square(source_array)
   output_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   output = 0
   index =  starting_point
 
